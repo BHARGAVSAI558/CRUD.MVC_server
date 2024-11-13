c92af3f78955eec174b3d089652c5236e3748a72
BHARGAVSAI558 <bhargavsai954@gmail.com>
2024-11-13 14:42:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,9 @@ const studentRoutes = require('./routes/studentRoutes');
 
 const app = express();
 
-app.use(cors(
- 
-));
+app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB using the connection string from environment variables
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
@@ -24,5 +21,5 @@ app.use('/api', studentRoutes);
 // Start the server on the specified port or default to 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(Server running on http://localhost:${PORT});
+  console.log(`Server running on http://localhost:${PORT}`);
 });
